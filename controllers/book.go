540ff45671ba11f2093540beee943e7de147feaf
Du Wei <perfuma@redfin.tech>
2025-02-28 11:32:11 +0800

    [book] update controller and add payload model

--- controllers/book.go
+++ controllers/book.go
@@ -4,147 +4,94 @@ import (
 	"library/initializers"
 	"library/models"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
-	"golang.org/x/crypto/bcrypt"
 )
 
 func GetBookList(c *gin.Context) {
 
-	var signUpPayload models.SignUpPayload
+	var books []models.Book
+	initializers.DB.Scan(&books)
 
-	if err := c.ShouldBindJSON(&signUpPayload); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	var userFound models.User
-	initializers.DB.Where("username=?", signUpPayload.Username).Find(&userFound)
-
-	if userFound.ID != 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "username already used"})
-		return
-	}
-
-	passwordHash, err := bcrypt.GenerateFromPassword([]byte(signUpPayload.Password), bcrypt.DefaultCost)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	user := models.User{
-		Nickname: signUpPayload.Nickname,
-		Username: signUpPayload.Username,
-		Password: string(passwordHash),
-	}
-
-	initializers.DB.Create(&user)
-
-	c.JSON(http.StatusOK, gin.H{"data": user})
+	c.JSON(http.StatusOK, gin.H{"data": books})
 
 }
 
 func BorrowBooks(c *gin.Context) {
 
-	var signUpPayload models.SignUpPayload
+	var books []models.Book
+	var BookPayload models.BookPayload
 
-	if err := c.ShouldBindJSON(&signUpPayload); err != nil {
+	if err := c.ShouldBindJSON(&BookPayload); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-
-	var userFound models.User
-	initializers.DB.Where("username=?", signUpPayload.Username).Find(&userFound)
-
-	if userFound.ID != 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "username already used"})
-		return
+	initializers.DB.Where("id in (?)", &BookPayload.ID).Scan(&books)
+	if len(books) == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "book not found"})
 	}
-
-	passwordHash, err := bcrypt.GenerateFromPassword([]byte(signUpPayload.Password), bcrypt.DefaultCost)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
+	for _,book := range books {
+		if book.Status != 1{
+			c.JSON(http.StatusNotFound, gin.H{"error": book.Name+" is not available"})
+		}
 	}
-
-	user := models.User{
-		Nickname: signUpPayload.Nickname,
-		Username: signUpPayload.Username,
-		Password: string(passwordHash),
+	for _,book := range books {
+		book.Status = 2
+		book.AvailableDate = time.Now().Add(28 * 24 * time.Hour)
+		initializers.DB.Save(book)
 	}
-
-	initializers.DB.Create(&user)
-
-	c.JSON(http.StatusOK, gin.H{"data": user})
-
+	c.JSON(http.StatusOK, gin.H{"data": books})
 }
 
 func ExtendBooks(c *gin.Context) {
 
-	var signUpPayload models.SignUpPayload
+	var books []models.Book
+	var BookPayload models.BookPayload
 
-	if err := c.ShouldBindJSON(&signUpPayload); err != nil {
+	if err := c.ShouldBindJSON(&BookPayload); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-
-	var userFound models.User
-	initializers.DB.Where("username=?", signUpPayload.Username).Find(&userFound)
-
-	if userFound.ID != 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "username already used"})
-		return
+	initializers.DB.Where("id in (?)", &BookPayload.ID).Scan(&books)
+	if len(books) == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "book not found"})
 	}
-
-	passwordHash, err := bcrypt.GenerateFromPassword([]byte(signUpPayload.Password), bcrypt.DefaultCost)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
+	for _,book := range books {
+		if book.Status == 1{
+			c.JSON(http.StatusNotFound, gin.H{"error": book.Name+" is not borrowed"})
+		}
 	}
-
-	user := models.User{
-		Nickname: signUpPayload.Nickname,
-		Username: signUpPayload.Username,
-		Password: string(passwordHash),
+	for _,book := range books {
+		book.AvailableDate = time.Now().Add(14 * 24 * time.Hour)
+		initializers.DB.Save(book)
 	}
-
-	initializers.DB.Create(&user)
-
-	c.JSON(http.StatusOK, gin.H{"data": user})
-
+	c.JSON(http.StatusOK, gin.H{"data": books})
 }
 
 func ReturnBooks(c *gin.Context) {
 
-	var signUpPayload models.SignUpPayload
+	var books []models.Book
+	var BookPayload models.BookPayload
 
-	if err := c.ShouldBindJSON(&signUpPayload); err != nil {
+	if err := c.ShouldBindJSON(&BookPayload); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-
-	var userFound models.User
-	initializers.DB.Where("username=?", signUpPayload.Username).Find(&userFound)
-
-	if userFound.ID != 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "username already used"})
-		return
+	initializers.DB.Where("id in (?)", &BookPayload.ID).Scan(&books)
+	if len(books) == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "book not found"})
 	}
-
-	passwordHash, err := bcrypt.GenerateFromPassword([]byte(signUpPayload.Password), bcrypt.DefaultCost)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
+	for _,book := range books {
+		if book.Status == 1{
+			c.JSON(http.StatusNotFound, gin.H{"error": book.Name+" is not borrowed"})
+		}
 	}
-
-	user := models.User{
-		Nickname: signUpPayload.Nickname,
-		Username: signUpPayload.Username,
-		Password: string(passwordHash),
+	for _,book := range books {
+		book.Status = 1
+		book.AvailableDate = time.Now()
+		initializers.DB.Save(book)
 	}
-
-	initializers.DB.Create(&user)
-
-	c.JSON(http.StatusOK, gin.H{"data": user})
+	c.JSON(http.StatusOK, gin.H{"data": books})
 
 }
--- models/book.go
+++ models/book.go
@@ -7,10 +7,14 @@ import (
 type Book struct {
 	ID        		uint       `json:"id" gorm:"primary_key"`
 	Name      		string     `json:"name"`	
-	Status    		uint	   // 1: available, 2: rent, 3: overdue
+	Status    		uint	   // 1: available, 2: rent
 	AvailableDate   time.Time  
 	UserID    		string     `json:"user_id" gorm:"index"` // Foreign key, indexed for performance
 	User      		User       `gorm:"foreignKey:UserID;references:ID"`
 	CreatedAt 		time.Time
 	UpdatedAt 		time.Time
+}
+
+type BookPayload struct {
+	ID []uint
 }
